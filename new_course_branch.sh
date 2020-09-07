1d67c35ed650fab6d9e262f46c1b5da7dfebba5b
Luis Ricardo Benhossi <ricardo.benhossi@gmail.com>
2020-09-07 08:59:44 -0300

    new_course_branch.sh file
    Create a sh file to help on creating aa new course branch.

---
+++ new_course_branch.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+echo "************************************************************"
+echo "*                                                          *"
+echo "*                 CREATE NEW COURSE BRANCH                 *"
+echo "*                                                          *"
+echo "************************************************************"
+echo
+echo
+
+echo "What is the name of the course?"
+read course_name
+echo
+
+echo "What is the order number of the course? (See the last course added in README file)"
+read course_order
+echo
+
+echo "What is the main folder or language folder of the course? (E.g.: Java/course or Python/course)"
+read course_folder_name
+echo
+
+course_branch_name=$course_order-$course_name
+base_branch_name="branch_base"
+container_branch_name="Container"
+
+echo
+echo "------------------------------------------------------------"
+echo
+echo "Creating a new course branch based on $base_branch_name"
+git branch -b $course_branch_name $base_branch_name
+
+echo
+echo "------------------------------------------------------------"
+echo
+echo "Importing container files from branch $container_branch_name (merging)"
+mkdir -p $course_folder_name/$course_branch_name
+cd $course_folder_name/$course_branch_name
+git merge container_branch_name
+
+echo
+echo "------------------------------------------------------------"
+echo "|                 PROCESS FINISHED. ENJOY!                 |"
+echo "------------------------------------------------------------"
+
