7b08437418029836e19b012e1d850f9e659c0971
Luis Ricardo Benhossi <ricardo.benhossi@gmail.com>
2020-09-30 20:22:29 +0000

    Class 05 finished. Course consluded.

--- Java/13-javaJPA_Part02/src/main/java/br/com/alura/jpa/dao/MovimentacaoDao.java
+++ Java/13-javaJPA_Part02/src/main/java/br/com/alura/jpa/dao/MovimentacaoDao.java
@@ -1,12 +1,18 @@
 package br.com.alura.jpa.dao;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import br.com.alura.jpa.modelo.MediaComData;
+import br.com.alura.jpa.modelo.Movimentacao;
 
 public class MovimentacaoDao {
 
@@ -14,6 +20,41 @@ public class MovimentacaoDao {
 
     public MovimentacaoDao(EntityManager em) {
         this.em = em;
+    }
+
+    public List<Movimentacao> getMovimentacoesFiltradasPorData(Integer filterDay, Integer filterMonth, Integer filterYear) {
+
+        CriteriaBuilder builder = em.getCriteriaBuilder();
+        CriteriaQuery<Movimentacao> query = builder.createQuery(Movimentacao.class);
+
+        Root<Movimentacao> root = query.from(Movimentacao.class);
+
+        // As we have more than one predicate, we need and array or list to store it.
+        List<Predicate> predicates = new ArrayList<>();
+
+        if (filterDay != null) {
+            // Predicates are the filter we do in our queries.
+            // So we need to compare the value on BD with the value we receive as a parameter. To do this we use builder.EQUAL
+            // As we'll use BD functions to filter the day, month and year from our date field (data) we must use
+            // builder funtions that receives the function you want to use, the class of data type, and the field we're using'
+            Predicate predicate = builder.equal(builder.function("day", Integer.class, root.get("data")), filterDay);
+            // As the scope of var predicate above is only here, we've to store it in our predicate list.
+            predicates.add(predicate);
+        }
+        if (filterMonth != null) {
+            Predicate predicate = builder.equal(builder.function("month", Integer.class, root.get("data")), filterMonth);
+            predicates.add(predicate);
+        }
+        if (filterYear != null) {
+            Predicate predicate = builder.equal(builder.function("year", Integer.class, root.get("data")), filterYear);
+            predicates.add(predicate);
+        }
+
+        query.where((Predicate[]) predicates.toArray(new Predicate[0]));
+
+        TypedQuery<Movimentacao> typedQuery = em.createQuery(query);
+
+        return typedQuery.getResultList();
     }
 
     public List<MediaComData> getMediaDiariaDasMovimentacoes() {
--- Java/13-javaJPA_Part02/src/main/java/br/com/alura/jpa/testes/TestaSomaDasMovimentacoes.java
+++ Java/13-javaJPA_Part02/src/main/java/br/com/alura/jpa/testes/TestaSomaDasMovimentacoes.java
@@ -5,8 +5,14 @@ import java.math.BigDecimal;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Root;
 
 import br.com.alura.jpa.dao.MovimentacaoDao;
+import br.com.alura.jpa.modelo.Movimentacao;
 
 public class TestaSomaDasMovimentacoes {
     public static void main(String[] args) {
@@ -24,6 +30,29 @@ public class TestaSomaDasMovimentacoes {
         // endregion
         BigDecimal somaDasMovimentacoes = new MovimentacaoDao(em).getSomaDasMovimentacoes();
         System.out.println("A soma de todas as movimentações registradas é de: " + somaDasMovimentacoes);
+
+        // NOTE Criteria Query
+        System.out.println("=".repeat(repeatSeparator));
+
+        // Criteria Builder is an interface, so we need to get a implementation of it.
+        // In this case, we'll get te EntityManager implementation.
+        CriteriaBuilder builder = em.getCriteriaBuilder();
+        // The Criteria query receives the class of the return of our query. Sum returns BigDecimal
+        CriteriaQuery<BigDecimal> query = builder.createQuery(BigDecimal.class);
+
+        // This is the main or ROOT of our query. It will be where the data comes from.
+        Root<Movimentacao> root = query.from(Movimentacao.class);
+
+        // Here we'll get the expression that executes the sum.
+        // Our builder will get the sum of root (Movimentacao.class) using the atribute VALOR
+        Expression<BigDecimal> sum = builder.sum(root.<BigDecimal>get("valor"));
+        // Now we passa the expression sum to perform the select.
+        query.select(sum);
+
+        // Done that, we pass the query to a typedquery to be executed by JPA.
+        TypedQuery<BigDecimal> typedQuery = em.createQuery(query);
+
+        System.out.println("(Criteria) A soma de todas as movimentações registradas é de: " + typedQuery.getSingleResult());
 
     }
 }
