b745da8835b99c0b7a82e24fb216aba6b7f1bfa9
Luis Ricardo Benhossi <ricardo.benhossi@gmail.com>
2020-12-26 12:02:26 -0300

    Class 06 (constructors and static members) finished. Course completed.

--- Java/02-Java_OO/bytebank-encapsulated/src/BankAccount.java
+++ Java/02-Java_OO/bytebank-encapsulated/src/BankAccount.java
@@ -8,6 +8,27 @@ public class BankAccount {
 	private int accountNumber;
 	private int agency;
 	private Client holder;
+//	When we use STATIC, it means that this attribute belongs to the class it self, not the instance of the class (object).
+//	When we does't use static, the attribute belongs to the object that we have instanced.
+//	So the use of THIS, always refers to the INSTANCE of the class.
+	private static int totalAccounts;
+	
+//	This is a constructor. It is a initialization routine that is responsible to build the object.
+//	This bellow is the default constructor java creates when you don't have any other constructor. By default it is hide.
+	public BankAccount() {
+		
+	}
+//	When you create a new constructor, the default one bellow is not available anymore. 
+//	So we can validate fields and receive data by default to build this object with certain obligated fields. 
+	public BankAccount(int agency, int accountNumber) {
+//		As the totalAccounts is static, we can't use this to refer to this attribute. Instead we use the class name. 
+		BankAccount.totalAccounts++; 
+		this.accountNumber = accountNumber;
+		this.agency = agency;
+		
+		System.out.println("Creating an account.");
+	}
+//	We can have more than one constructor. 
 	
 	public void deposit(double value) {
 //		Keyword THIS is to make explicit that the attribute BALANCE we are working is referenced 
@@ -39,6 +60,10 @@ public class BankAccount {
 	}
 	
 	public void setAccountNumber(int accountNumber) {
+		if (accountNumber <= 0) {
+			System.out.println("The Account Number msut be higher than 0.");
+			return;
+		}
 		this.accountNumber = accountNumber;
 	}
 	
@@ -47,6 +72,10 @@ public class BankAccount {
 	}
 	
 	public void setAgency(int agency) {
+		if (agency <= 0) {
+			System.out.println("The Agency msut be higher than 0.");
+			return;
+		}
 		this.agency = agency;
 	}
 	
@@ -60,5 +89,13 @@ public class BankAccount {
 
 	public void setHolder(Client holder) {
 		this.holder = holder;
+	}
+	
+
+	public static int getTotalAccounts() {
+//		As we see, the THIS makes a reference to the instantiation of a class and is not available on a static method because
+//		it belong to the class and not the instances of it.
+//		System.out.println(this.balance);
+		return totalAccounts;
 	}
 }
